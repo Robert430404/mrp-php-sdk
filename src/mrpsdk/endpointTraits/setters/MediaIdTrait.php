ea50d74e8af6cf942b0e49bca8d191eba42b3195
Robert Cox <rcox@dontbebroke.com>
2016-09-06 13:33:40 -0700

    Implemented Media Single Endpoint With Media ID Trait

---
+++ src/mrpsdk/endpointTraits/setters/MediaIdTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace mrpsdk\endpointTraits\setters;
+
+trait MediaIdTrait
+{
+    /**
+     * Sets the media id for the api call
+     *
+     * @param  integer $id
+     * @return         $this
+     */
+    public function setMediaId($id = 0)
+    {
+        if (isset($this->requestData['MediaId'])) {
+            unset($this->requestData['MediaId']);
+        }
+
+        $this->requestData = array_merge($this->requestData, ['MediaId' => $id]);
+
+        return $this;
+    }
+}
