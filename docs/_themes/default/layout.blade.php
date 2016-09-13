c27c1673f99554ce22e93103d9ce39d579f4d351
Robert Cox <rcox@dontbebroke.com>
2016-09-13 14:14:30 -0700

    Changed Syntax Highlight Theme

--- docs/_themes/default/layout.blade.php
+++ docs/_themes/default/layout.blade.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="{{ $config['base_url'] . $config['css_path'] . 'reset.css' }}" />
         <link rel="stylesheet" href="{{ $config['base_url'] . $config['css_path'] . 'index.css' }}" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/themes/prism.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/themes/prism-twilight.min.css">
     </head>
 
     <body>
