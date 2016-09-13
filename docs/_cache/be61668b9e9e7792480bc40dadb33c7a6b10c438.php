b2ad422b5dd0489cde94fbb4ead054c40df9a2d2
Robert Cox <rcox@dontbebroke.com>
2016-09-13 09:54:10 -0700

    Refinements To CSS And Adjusted PHPUnit To Not Test Docs

---
+++ docs/_cache/be61668b9e9e7792480bc40dadb33c7a6b10c438.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo e($title ? $title . ' - ' : ''); ?><?php echo e($config['site_title']); ?></title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo e($config['base_url'] . $config['css_path'] . 'reset.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo e($config['base_url'] . $config['css_path'] . 'index.css'); ?>" />
+        <script src="<?php echo e($config['base_url'] . $config['js_path'] . 'index.js'); ?>"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="sidebar">
+                <?php echo $__env->make('sidebar', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+            </div>
+
+            <div class="content">
+                <?php echo $__env->yieldContent('content'); ?>
+            </div>
+        </div>
+    </body>
+</html>
