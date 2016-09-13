835bfe07b8364247560794960d05567835810f2a
Robert Cox <rcox@dontbebroke.com>
2016-09-12 17:02:31 -0700

    Additional Refinements

--- docs/_cache/479782607f078a81541e83922de9e0a435fc97ae.php
+++ docs/_cache/479782607f078a81541e83922de9e0a435fc97ae.php
@@ -3,7 +3,9 @@
     <head>
         <title><?php echo e($title ? $title . ' - ' : ''); ?><?php echo e($config['site_title']); ?></title>
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo e($config['base_url'] . $config['css_path'] . 'reset.css'); ?>" />
         <link rel="stylesheet" href="<?php echo e($config['base_url'] . $config['css_path'] . 'index.css'); ?>" />
+        <script src="<?php echo e($config['base_url'] . $config['js_path'] . 'index.js'); ?>"></script>
     </head>
 
     <body>
--- docs/_themes/default/layout.blade.php
+++ docs/_themes/default/layout.blade.php
@@ -3,7 +3,9 @@
     <head>
         <title>{{ $title ? $title . ' - ' : '' }}{{ $config['site_title'] }}</title>
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link rel="stylesheet" href="{{ $config['base_url'] . $config['css_path'] . 'reset.css' }}" />
         <link rel="stylesheet" href="{{ $config['base_url'] . $config['css_path'] . 'index.css' }}" />
+        <script src="{{ $config['base_url'] . $config['js_path'] . 'index.js' }}"></script>
     </head>
 
     <body>
