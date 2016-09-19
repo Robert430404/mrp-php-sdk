b26bdfc5601613f943bc4202fd74e26bda5b86e4
Robert Joseph Cox Jr <robert@studiorclv.com>
2016-09-18 18:44:15 -0700

    Additional Pages Generated For The Documentation

--- docs/_cache/0cc8b0d8461f1b5a40e6c84aeedbefca88050607.php
+++ docs/_cache/0cc8b0d8461f1b5a40e6c84aeedbefca88050607.php
@@ -1,19 +1,19 @@
 <h2 class="sidebar-title">Endpoints</h2>
 <ul class="endpoints">
     <li class="endpoint-title">
-        <a href="{{ $config['base_url'] }}">Overview</a>
+        <a href="<?php echo e($config['base_url']); ?>">Overview</a>
     </li>
     <li class="endpoint-title" onclick="showLinks(this)">
         News
         <ul class="endpoint-menu">
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'news' }}">/v2/news/</a>
+                <a href="<?php echo e($config['base_url'] . 'news'); ?>">/v2/news/</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'news/single' }}">/v2/news/?NewsId={id}</a>
+                <a href="<?php echo e($config['base_url'] . 'news/single'); ?>">/v2/news/?NewsId={id}</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'news/views' }}">/v2/news/views/</a>
+                <a href="<?php echo e($config['base_url'] . 'news/views'); ?>">/v2/news/views/</a>
             </li>
         </ul>
     </li>
@@ -21,34 +21,34 @@
         Schedules/Results
         <ul class="endpoint-menu">
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'schedules' }}">/v2/schedules/</a>
+                <a href="<?php echo e($config['base_url'] . 'schedules'); ?>">/v2/schedules/</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'schedules/years' }}">/v2/schedules/years/</a>
+                <a href="<?php echo e($config['base_url'] . 'schedules/years'); ?>">/v2/schedules/years/</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'schedules/events' }}">/v2/schedules/events/</a>
+                <a href="<?php echo e($config['base_url'] . 'schedules/events'); ?>">/v2/schedules/events/</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'schedules/event' }}">/v2/schedules/events/?EventId={id}</a>
+                <a href="<?php echo e($config['base_url'] . 'schedules/event'); ?>">/v2/schedules/events/?EventId={id}</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'schedules/results' }}">/v2/schedules/results/</a>
+                <a href="<?php echo e($config['base_url'] . 'schedules/results'); ?>">/v2/schedules/results/</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'schedules/lineups' }}">/v2/schedules/lineups/</a>
+                <a href="<?php echo e($config['base_url'] . 'schedules/lineups'); ?>">/v2/schedules/lineups/</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'schedules/feed' }}">/v2/schedules/results/feed/</a>
+                <a href="<?php echo e($config['base_url'] . 'schedules/feed'); ?>">/v2/schedules/results/feed/</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'schedules/driver-stats' }}">/v2/schedules/driverStats/</a>
+                <a href="<?php echo e($config['base_url'] . 'schedules/driver-stats'); ?>">/v2/schedules/driverStats/</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'schedules/driver-passes' }}">/v2/schedules/driverPasses/</a>
+                <a href="<?php echo e($config['base_url'] . 'schedules/driver-passes'); ?>">/v2/schedules/driverPasses/</a>
             </li>
             <li class="endpoint">
-                <a href="{{ $config['base_url'] . 'schedules/entry-list' }}">/v2/schedules/entryList/</a>
+                <a href="<?php echo e($config['base_url'] . 'schedules/entry-list'); ?>">/v2/schedules/entryList/</a>
             </li>
         </ul>
     </li>
---
+++ docs/_cache/b854044b16eb74f09eaec36f668e33fd658d5d5a.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo e($title ? $title . ' - ' : ''); ?><?php echo e($config['site_title']); ?></title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo e($config['base_url'] . $config['css_path'] . 'reset.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo e($config['base_url'] . $config['css_path'] . 'index.css'); ?>" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/themes/prism.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/themes/prism-twilight.min.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="sidebar">
+                <?php echo $__env->make('sidebar', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+            </div>
+
+            <div class="content">
+                <?php echo $__env->yieldContent('content'); ?>
+            </div>
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/prism.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/components/prism-bash.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/components/prism-json.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.1/components/prism-php.min.js"></script>
+        <script src="<?php echo e($config['base_url'] . $config['js_path'] . 'index.js'); ?>"></script>
+    </body>
+</html>
